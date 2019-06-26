df9abfea518e0d1b888a0ad9f3732da70741289d
Guilherme C. Souza <gcsgacc@gmail.com>
2019-06-26 17:18:42 -0300

    Change code to auto-generate http method functions
    - Since their behavior must be the same anyway

--- test/spec.js
+++ test/spec.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const muhb = require('../lib/index.js');
 const HTTPBIN_URL = process.env.HTTPBIN_URL || 'http://localhost';
 
-describe('::get(url[, headers])', function(){
+describe('Verbs', function(){
 
     it('should properly reach a GET endpoint', async function(){
         let r = await muhb.get(HTTPBIN_URL + '/get');
@@ -12,20 +12,12 @@ describe('::get(url[, headers])', function(){
         assert.equal(typeof o.args, 'object');
     });
 
-});
-
-describe('::head(url[, headers])', function(){
-
     it('should properly reach a GET endpoint', async function(){
         let r = await muhb.head(HTTPBIN_URL + '/get');
         assert.equal(r.status, 200);
         assert.equal(typeof r.headers, 'object');
         assert.equal(r.body, '');
     });
-
-});
-
-describe('::post(url[, headers][, body])', function(){
 
     it('should properly reach a POST endpoint', async function(){
         let r = await muhb.post(HTTPBIN_URL + '/post', 'balela');
@@ -38,10 +30,6 @@ describe('::post(url[, headers][, body])', function(){
         assert.equal(o.headers['Thak-Thek'], 'that');
     });
 
-});
-
-describe('::put(url[, headers][, body])', function(){
-
     it('should properly reach a PUT endpoint', async function(){
         let r = await muhb.put(HTTPBIN_URL + '/put', 'balela');
         assert.equal(r.status, 200);
@@ -52,10 +40,6 @@ describe('::put(url[, headers][, body])', function(){
         o = JSON.parse(r.body);
         assert.equal(o.headers['Thak-Thek'], 'that');
     });
-
-});
-
-describe('::patch(url[, headers][, body])', function(){
 
     it('should properly reach a PATCH endpoint', async function(){
         let r = await muhb.patch(HTTPBIN_URL + '/patch', 'balela');
@@ -68,10 +52,6 @@ describe('::patch(url[, headers][, body])', function(){
         assert.equal(o.headers['Thak-Thek'], 'that');
     });
 
-});
-
-describe('::delete(url[, headers])', function(){
-
     it('should properly reach a DELETE endpoint', async function(){
         let r = await muhb.delete(HTTPBIN_URL + '/delete');
         assert.equal(r.status, 200);
@@ -79,10 +59,6 @@ describe('::delete(url[, headers])', function(){
         let o = JSON.parse(r.body);
         assert.equal(typeof o.args, 'object');
     });
-
-});
-
-describe('::options(url[, headers])', function(){
 
     it('should properly reach a GET endpoint', async function(){
         let r = await muhb.options(HTTPBIN_URL + '/get');
